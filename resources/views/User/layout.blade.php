d0d5db592f6e7253e72f9a35fde864017abfc40d
MuhammadNasir1 <nmian7080@gmail.com>
2024-12-27 19:43:59 +0500

    change navbar

--- resources/views/User/layout.blade.php
+++ resources/views/User/layout.blade.php
@@ -40,9 +40,9 @@
     </div>
 
     <nav
-        class="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600 shadow-lg">
+        class="bg-white dark:bg-gray-900 fixed w-full z-[100] top-0 start-0 border-b border-gray-200 dark:border-gray-600 shadow-lg">
         <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-            <a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
+            <a href="../" class="flex items-center space-x-3 rtl:space-x-reverse">
                 <img src="{{ asset('assets/images/logo.png') }}" class="h-14" alt="Logo">
                 {{-- <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">mg</span> --}}
             </a>
@@ -64,16 +64,16 @@
                     class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 ">
                     <li>
                         <a href="../"
-                            class="block py-2 px-3  rounded md:bg-transparent  md:p-0 {{ request()->is('/') ? ' bg-primary text-primary' : 'text-gray-900 ' }} "
+                            class="block py-2 px-3  rounded md:bg-transparent  md:p-0 {{ request()->is('/') ? ' bg-primary md:text-primary text-white' : 'text-gray-900 ' }} "
                             aria-current="page">Home</a>
                     </li>
                     <li>
                         <a href="../menu"
-                            class="block py-2 px-3  rounded md:bg-transparent  md:p-0 {{ request()->is('menu') ? ' bg-primary text-primary' : 'text-gray-900 ' }} ">Menu</a>
+                            class="block py-2 px-3  rounded md:bg-transparent  md:p-0 {{ request()->is('menu') ? ' bg-primary md:text-primary text-white' : 'text-gray-900 ' }} ">Menu</a>
                     </li>
                     <li>
                         <a href="../contact"
-                            class="block py-2 px-3  rounded md:bg-transparent  md:p-0 {{ request()->is('contact') ? ' bg-primary text-primary' : 'text-gray-900 ' }}  ">Contact</a>
+                            class="block py-2 px-3  rounded md:bg-transparent  md:p-0 {{ request()->is('contact') ? ' bg-primary md:text-primary text-white' : 'text-gray-900 ' }}  ">Contact</a>
                     </li>
 
                 </ul>
